0bbe58e079429f6ad3ee0d457e5d40e0ab9a2e8c
YutaMiyake <ymiyake@nevada.unr.edu>
2015-10-30 00:57:20 -0700

    graphviz

---
+++ graph.cpp
@@ -0,0 +1,40 @@
+#include "graph.h"
+/**
+@file graph.cpp
+@author Yuta Miyake
+@brief Creates a dot file of simple tree for graphviz
+*/
+Graph::Graph(std::string filename, std::string graph_name){
+  // parameterized constructor w/ default arguments
+  this->filename = filename;
+  this->graph_name = graph_name;
+  this->visualizer.setFileName(this->filename);
+}
+Graph::~Graph(){
+  // destructor
+    this->visualizer.debug("}");
+}
+void Graph::setVisualizer(bool on_off){
+  // on/off visualizer
+  this->visualizer.setDebug(on_off);
+}
+void Graph::startBuild(){
+  // start building the dot file
+  std::stringstream ss;
+  this->visualizer.setFileName(this->filename);
+  this->visualizer.setDebug(true);
+  ss << "digraph " << this->graph_name << " {" << std::endl;
+  this->visualizer.debug(ss.str());
+}
+void Graph::addNode(int id, std::string label){
+  // add new node into the dot file
+  std::stringstream ss;
+  ss << id << " [label=\"" << label << "\"];" << std::endl;
+  this->visualizer.debug(ss.str());
+}
+void Graph::addEdge(int pid, int cid){
+  // add new edge between two nodes into the dot file
+  std::stringstream ss;
+  ss << pid << " -> " << cid << ";" << std::endl;
+  this->visualizer.debug(ss.str());
+}
---
+++ graph.h
@@ -0,0 +1,18 @@
+#ifndef __GRAPH__H__
+#define __GRAPH__H__
+#include "Debugger.h"
+#include <sstream>
+
+class Graph{
+    public:
+        Graph(std::string filename = "graph.dot",std::string graph_name = "Graph");
+        ~Graph();
+        void setVisualizer(bool on_off);
+        void startBuild();
+        void addEdge(int pid, int cid);
+        void addNode(int id, std::string label);
+    private:
+      Debugger visualizer;
+      std::string filename, graph_name;
+};
+#endif
