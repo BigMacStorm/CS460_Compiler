c639789abeedb7b2a9c6e6006d9e72176bc345f4
niki-s <niki.silveria@gmail.com>
2015-11-04 18:00:52 -0800

    added test cases for array, if, and a more complicated array_if

--- testcases/array.c
+++ testcases/array.c
@@ -1,23 +1,12 @@
 int arr2[4] = {4, 3, 2, 1};
-/*char arr[2][3];
+char arr[2][3];
 
-void test(float);
 
 int main() {
     int a = 0;
-    a = arr2[2];
+    int b = 0;
     
-    test(4.0);
+    a = arr2[2];
+    b = arr[1][1];
     return 0;
-}
-
-void test(float in) {
-    float mine = in;
-}
-*/
-
-int main(){
-    int a = 0;
-    if (arr2[a] == 4)
-        return 0;
 }
---
+++ testcases/array_if.c
@@ -0,0 +1,8 @@
+int arr2[4] = {4, 3, 2, 1};
+
+int main(){
+    int a = 0;
+    if (arr2[a] == 4){
+        return 0;
+    }
+}
