1b80fdcdc81c513648f1d747a5137f780d663485
niki-s <niki.silveria@gmail.com>
2015-11-04 17:55:38 -0800

    added a few nice test cases for things

---
+++ testcases/array.c
@@ -0,0 +1,23 @@
+int arr2[4] = {4, 3, 2, 1};
+/*char arr[2][3];
+
+void test(float);
+
+int main() {
+    int a = 0;
+    a = arr2[2];
+    
+    test(4.0);
+    return 0;
+}
+
+void test(float in) {
+    float mine = in;
+}
+*/
+
+int main(){
+    int a = 0;
+    if (arr2[a] == 4)
+        return 0;
+}
---
+++ testcases/if.c
@@ -0,0 +1,11 @@
+int main (){
+    int a = 0;
+    if(a != 0){
+        return a;
+    }
+    else if(a != 1){
+        return 1;
+    }
+    else
+        return 0;
+}
