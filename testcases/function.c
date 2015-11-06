55856581f756422a1780114e8486826e85347793
niki-s <niki.silveria@gmail.com>
2015-11-05 19:25:46 -0800

    nice test cases for functions and warnings

---
+++ testcases/function.c
@@ -0,0 +1,17 @@
+int floatToInt(float f);
+
+int main(){
+    float f = 1.0;
+    float d;
+    d = floatToInt(f);
+    return 0;    
+}
+
+int floatToInt(float f)
+{
+    int i = 0;
+    i = f;
+    
+    return i;
+}
+
