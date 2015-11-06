3c8650a4d35ab21a97c934ccbf5289f03bde1430
YutaMiyake <ymiyake@nevada.unr.edu>
2015-11-06 14:53:24 -0800

    modify bubble sort

--- testcases/bubble.c
+++ testcases/bubble.c
@@ -1,12 +1,7 @@
-void swap(int* item1, int* item2);
 void bubble(int array[], int size);
-
 int main(){
-  int idx = 0;
-  const int SIZE = 10;
-  int array[SIZE] = {6,-128,100,7,-2,0,21,85,21,54};
-
-  bubble(array, SIZE);
+  int array[10] = {6,-128,100,7,-2,0,21,85,21,54};
+  bubble(array, 10);
   return 0;
   //!!S
 }
@@ -14,18 +9,15 @@ int main(){
 void bubble(int array[], int size){
   int idx1 = 0;
   int idx2 = 0;
+  int temp;
   for(idx1 = 0; idx1 < size-1; idx1++){
     for(idx2 = 0; idx2 < size-idx1-1; idx2++){
       if(array[idx2] > array[idx2+1]){
-        swap(&array[idx2], &array[idx2+1]);
+        temp = array[idx2];
+        array[idx2] = array[idx2+1];
+        array[idx2+1] = temp;
       }
     }
   }
-  //!!S
-}
-void swap(int* item1, int* item2){
-  int temp = *item1;
-  *item1 = *item2;
-  *item2 = temp;
   //!!S
 }
