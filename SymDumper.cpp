7c44bb5eb3454443a98451fbaf694663e006808b
YutaMiyake <ymiyake@nevada.unr.edu>
2015-12-11 17:11:02 -0800

    add symbol-nodes dumper

---
+++ SymDumper.cpp
@@ -0,0 +1,16 @@
+#include "SymDumper.h"
+
+void SymDumper::addSymToDump(SymbolNode* sym){
+  std::vector<SymbolNode*>::iterator iter;
+  iter = find (this->symDump.begin(), this->symDump.end(), sym);
+    if (iter == this->symDump.end()){
+      this->symDump.push_back(sym);
+    }
+}
+void SymDumper::clearSymDump(){
+  std::vector<SymbolNode*>::iterator iter;
+  for(iter = this->symDump.begin(); iter != this->symDump.end(); ++iter){
+    //std::cout << (*iter)->getName() << " is freed" << std::endl;
+    delete *iter;
+  }
+}
