3dc9c9fb5b420091dd0ab23708f579f352a4a982
YutaMiyake <ymiyake@nevada.unr.edu>
2015-12-13 01:17:43 -0800

    change 3darray testcase

--- testcases/3acTests/3darray.c
+++ testcases/3acTests/3darray.c
@@ -9,5 +9,15 @@ int main(){
     }
   }
 
-  arr[1][2][3] = arr[9][9][9] * 2;
+  for(idx1 = 0; idx1 < 10; ++idx1){
+    for(idx2 = 0; idx2 < 10; ++idx2){
+      for(idx3 = 0; idx3 < 10; ++idx3){
+        print_int(idx1*idx2*idx3);
+      }
+    }
+  }
+
+
+  arr[0][0][0] = arr[9][9][9] * arr[9][9][8];
+  print_int(arr[0][0][0]);
 }
